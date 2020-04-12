a4f17d27bb415e4d719b569f4e82778b424d4a8d
Santosh Marigowda <santosharakere@gmail.com>
2020-04-12 15:38:17 +0100

    validating phone number

---
+++ projects/form-validation/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  onSubmit(value: any) {
+    console.log('form submitted.');
+    console.log(value);
+  }
+}
