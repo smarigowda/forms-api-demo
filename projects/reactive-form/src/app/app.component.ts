a472f2804212938319f6110f54d43d875ecf0553
Santosh Marigowda <santosharakere@gmail.com>
2020-04-12 10:33:01 +0100

    added bulms css

--- projects/reactive-form/src/app/app.component.ts
+++ projects/reactive-form/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'reactive-form';
+  myFormModel: FormGroup;
+  constructor() {
+    this.myFormModel = new FormGroup({
+      username: new FormControl(''),
+      ssn: new FormControl(''),
+      passwordsGroup: new FormGroup({
+        password: new FormControl(''),
+        pconfirm: new FormControl('')
+      }),
+      dateRange: new FormGroup({
+        from: new FormControl(''),
+        to: new FormControl('')
+      })
+    })
+  }
+  onSubmit() {
+    console.log('Form submitted...');
+    console.log(this.myFormModel);
+  }
 }
