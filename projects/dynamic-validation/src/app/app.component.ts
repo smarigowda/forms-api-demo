8cbfe4c219a9207134f465ba51c6f2160a6b91af
Santosh Marigowda <santosharakere@gmail.com>
2020-04-12 22:08:46 +0100

    dynamic validation

--- projects/dynamic-validation/src/app/app.component.ts
+++ projects/dynamic-validation/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'dynamic-validation';
+export class AppComponent implements OnInit {
+  myFormModel: FormGroup;
+  countryControl: FormControl;
+  phoneControl: FormControl;
+  constructor(fb: FormBuilder) {
+    this.myFormModel = fb.group({
+      country: [''],
+      phone: ['']
+    });
+  }
+  onSubmit() {
+    console.log('submit clicked...!');
+  }
+  ngOnInit() {
+    this.countryControl = this.myFormModel.get('country') as FormControl;
+    this.phoneControl = this.myFormModel.get('phone') as FormControl;
+
+    this.countryControl.valueChanges.subscribe(country => {
+      console.log(country);
+      if(country === 'USA') {
+        this.phoneControl.setValidators([Validators.minLength(10)]);
+      } else {
+        this.phoneControl.setValidators([Validators.minLength(11)]);
+      }
+      this.phoneControl.updateValueAndValidity();
+    })
+  }
 }
